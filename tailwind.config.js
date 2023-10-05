bcf96eade7122fa2b49e33785ec8ad8a49499529
Grazvydas Marevicius <saiderr220@gmail.com>
2023-10-05 14:26:52 -0500

    added custom colors and fonts to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 module.exports = {
   content: ["./dist/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'purple-hero': "#6018F7",
+        'teal': "#5EEBA7"
+      },
+      fontFamily: {
+        'poppins': ['Poppins', 'sans'], 
+        'roboto': ['Roboto', 'sans'], 
+      }
+    },
   },
   plugins: [],
 }
